6617b9e92031f76da16e93f1ff15aaea941b26e3
Hucketa <gunenkoym@gmail.com>
2026-05-07 19:01:07 +0300

    rework(task4): -change portfolio page structure -added styles for the cards -added code for filters in script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function toggleMobileNav() {
     mobileNav.classList.toggle('active');
     toggleButton.classList.toggle('active');
 }
-// Закрити меню при кліку на посилання
+
 document.addEventListener('DOMContentLoaded', function () {
     const mobileNavLinks =
         document.querySelectorAll('.mobile-nav a');
@@ -15,6 +15,29 @@ document.addEventListener('DOMContentLoaded', function () {
             const toggleButton = document.querySelector('.mobile-nav-toggle');
             mobileNav.classList.remove('active');
             toggleButton.classList.remove('active');
+        });
+    });
+});
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const portfolioItems = document.querySelectorAll(".portfolio-item");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        filterButtons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+        button.classList.add("active");
+        const filter = button.dataset.filter;
+        portfolioItems.forEach(item => {
+            if (
+                filter === "all" ||
+                item.dataset.category.includes(filter)
+            ) {
+                item.style.display = "flex";
+            } else {
+                item.style.display = "none";
+            }
         });
     });
 });
